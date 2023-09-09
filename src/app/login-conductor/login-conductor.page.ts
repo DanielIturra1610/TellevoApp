332bfc40f36a8d6dca3d16bcb5910be27eeb4ba9
h-garrido <hernanjorge.garrido@gmail.com>
2023-09-09 00:05:41 -0300

    enruta login conductor y pasajero

--- src/app/login-conductor/login-conductor.page.ts
+++ src/app/login-conductor/login-conductor.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-login-conductor',
@@ -7,9 +9,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginConductorPage implements OnInit {
 
-  constructor() { }
+  usuarios: { [key: string]: string } = {
+    conductor1: '456def1',
+    conductor2: '456def2',
+    conductor3: '456def3',
+  };
+
+  usuario: string = '';
+  password: string = '';  
+
+  constructor(private router: Router, private alertController: AlertController) { }
 
   ngOnInit() {
   }
 
+  async login() {
+    console.log('Usuario:', this.usuario);
+    console.log('Contraseña:', this.password);
+  
+    const contraseñaCorrecta = this.usuarios[this.usuario];
+    console.log('Contraseña correcta:', contraseñaCorrecta);
+  
+    if (contraseñaCorrecta && contraseñaCorrecta === this.password) {
+      console.log('Inicio de sesión exitoso');
+      this.router.navigate(['/home']);
+    } else {
+      console.log('Credenciales incorrectas');
+      const alert = await this.alertController.create({
+        header: 'Error de inicio de sesión',
+        message: 'Las credenciales no son válidas. Inténtelo nuevamente.',
+        buttons: ['Aceptar'],
+      });
+      await alert.present();
+    }
+  }
 }
--- src/app/login-pasajero/login-pasajero.page.ts
+++ src/app/login-pasajero/login-pasajero.page.ts
@@ -9,11 +9,39 @@ import { AlertController } from '@ionic/angular';
 })
 export class LoginPasajeroPage implements OnInit {
 
+  usuarios: { [key: string]: string } = {
+    pasajero1: '123abc1',
+    pasajero2: '123abc2',
+    pasajero3: '123abc3',
+  };
+
+  usuario: string = '';
+  password: string = '';  
+
   constructor(private router: Router, private alertController: AlertController) { }
 
   ngOnInit() {
   }
 
+  async login() {
+    console.log('Usuario:', this.usuario);
+    console.log('Contraseña:', this.password);
   
-
+    const contraseñaCorrecta = this.usuarios[this.usuario];
+    console.log('Contraseña correcta:', contraseñaCorrecta);
+  
+    if (contraseñaCorrecta && contraseñaCorrecta === this.password) {
+      console.log('Inicio de sesión exitoso');
+      this.router.navigate(['/home']);
+    } else {
+      console.log('Credenciales incorrectas');
+      const alert = await this.alertController.create({
+        header: 'Error de inicio de sesión',
+        message: 'Las credenciales no son válidas. Inténtelo nuevamente.',
+        buttons: ['Aceptar'],
+      });
+      await alert.present();
+    }
+  }
+  
 }
