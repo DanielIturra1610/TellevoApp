184a00b8990a2548cfcf758a7073262e0dfb441e
h-garrido <hernanjorge.garrido@gmail.com>
2023-09-27 18:47:57 -0300

    agrega logica a guard

--- src/app/ingresado.guard.ts
+++ src/app/ingresado.guard.ts
@@ -1,15 +1,25 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
 import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class IngresadoGuard implements CanActivate {
+
+  constructor(private router: Router) {}
+
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return true;
-  }
-  
+      
+      const datosUsuario = route.queryParams['datosUsuario'];
+
+      if (datosUsuario && (datosUsuario.startsWith('pasajero') || datosUsuario.startsWith('conductor'))) {
+        return true;
+      } else {
+        this.router.navigate(['/login-general']);
+        return false;
+      }
+  } 
 }
